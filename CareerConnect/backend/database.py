acb85c4ba33201a1c8652f20f552c6f67e8ad023
Gopinath G <gajagopi2006@gmail.com>
2026-06-20 07:01:42 +0530

    initialize backend project

--- CareerConnect/backend/database.py
+++ CareerConnect/backend/database.py
@@ -2,11 +2,10 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./careerconnect.db"
+SQLALCHEMY_DATABASE_URL = "postgresql://postgres:careerconnect123213@db.krwlwfolnoakexjedfhy.supabase.co:5432/postgres"
 
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
-)
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+    
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
